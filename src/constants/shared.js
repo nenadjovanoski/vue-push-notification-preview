04d1da36fe367f921a745ee0d55904e7e7afaa7e
Nenad Jovanoski <jovanoskinenad@gmail.com>
2020-11-26 21:24:19 +0100

    v1

--- src/constants/shared.js
+++ src/constants/shared.js
@@ -2,3 +2,13 @@ export const DEVICE = Object.freeze({
     IPHONE_X: 'iphone_x',
     PIXEL_4: 'pixel_4'
 });
+
+export const APPEARANCE_MODE = Object.freeze({
+    LIGHT: 'light',
+    DARK: 'dark'
+});
+
+export const SIZE_MODE = Object.freeze({
+    INITIAL: 'initial',
+    EXPANDED: 'expanded'
+});
