fbce3b7cebafc5de910197e5cbd9390d070ee5d7
Nenad Jovanoski <jovanoskinenad@gmail.com>
2020-12-02 20:48:11 +0100

    Export device constants (VPNP_APPEARANCE_MODE and VPNP_SIZE)

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 export { default as IphonePreview } from './IphonePreview.vue';
 export { default as AndroidPreview } from './AndroidPreview.vue';
+
+export {
+    APPEARANCE_MODE as VPNP_APPEARANCE_MODE,
+    SIZE as VPNP_SIZE
+} from '../constants/shared';
