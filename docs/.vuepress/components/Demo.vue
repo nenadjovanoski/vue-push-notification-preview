6b2d391e31e41ba8da1e1952071138f39f9f363a
Nenad Jovanoski <jovanoskinenad@gmail.com>
2020-12-09 20:38:10 +0100

    Vuepress Demo and SourceCode components

---
+++ docs/.vuepress/components/Demo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <component :is="component"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                component: null
+            }
+        },
+
+        props: {
+            componentName: {
+                type: String,
+                required: true
+            }
+        },
+
+        mounted () {
+            import(`../components/${this.componentName}`).then(module => {
+                this.component = module.default;
+            });
+        }
+    }
+</script>
