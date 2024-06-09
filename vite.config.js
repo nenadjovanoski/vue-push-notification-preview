c2c62fa32f652b7c9355a50a5b8e6e462cc277bb
Nenad Jovanoski <jovanoskinenad@gmail.com>
2024-06-09 17:28:29 +0100

    Build for Vue.js v3

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/components/index.js'),
+      name: 'vue-push-notification-preview',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
