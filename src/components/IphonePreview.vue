22152878b7122e95027006ee0b1a0aaaf86d1105
Nenad Jovanoski <jovanoskinenad@gmail.com>
2024-06-09 17:11:38 +0100

    Replace the mixin and use composition API on components

--- src/components/IphonePreview.vue
+++ src/components/IphonePreview.vue
@@ -60,53 +60,82 @@
     </div>
 </template>
 
-<script>
-    import { DEVICE_SIZE } from '../constants/shared';
+<script setup>
+    import { APPEARANCE_MODE, DEVICE_SIZE } from '../constants/shared';
+    import { useDevice } from './use-device';
+    import { computed, toRef } from 'vue';
 
-    import { deviceMixin } from '../mixins/deviceMixin';
-
-    export default {
-        name: 'IphonePreview',
-
-        mixins: [
-            deviceMixin
-        ],
-
-        props: {
-            textApplicationName: {
-                type: String,
-                default: 'App name'
-            },
-
-            textTime: {
-                type: String,
-                default: '1h ago',
-                validator: value => value.length < 8
-            },
-
-            textTitle: {
-                type: String,
-                default: 'Title notification'
-            },
-
-            textBody: {
-                type: String,
-                default: ''
-            },
-
-            height: {
-                type: Number,
-                default: DEVICE_SIZE.IPHONE_X_HEIGHT
-            }
+    const props = defineProps({
+        textApplicationName: {
+            type: String,
+            default: 'App name'
         },
 
-        computed: {
-            sizeStyle() {
-                return {
-                    height: `${this.height}px`,
-                    width: `${this.height * (DEVICE_SIZE.IPHONE_X_WIDTH / DEVICE_SIZE.IPHONE_X_HEIGHT)}px`
-                }
-            }
-        }
-    }
+        textTime: {
+            type: String,
+            default: '1h ago',
+            validator: value => value.length < 8
+        },
+
+        textTitle: {
+            type: String,
+            default: 'Title notification'
+        },
+
+        textBody: {
+            type: String,
+            default: ''
+        },
+
+        height: {
+            type: Number,
+            default: DEVICE_SIZE.IPHONE_X_HEIGHT
+        },
+
+        image: {
+            type: String,
+            default: ''
+        },
+
+        backgroundImage: {
+            type: String,
+            default: ''
+        },
+
+        backgroundColor: {
+            type: String,
+            default: '#c1c1c1'
+        },
+
+        appearanceMode: {
+            type: String,
+            default: APPEARANCE_MODE.LIGHT,
+            validator: value => Object.values(APPEARANCE_MODE).indexOf(value) !== -1
+        },
+    });
+
+    const {
+        notificationImageStyle,
+        backgroundStyle,
+        appearanceModeClass,
+        hasTitle,
+        hasBody,
+        hasImage,
+        isExpanded,
+        notificationExpandedClass,
+    } = useDevice({
+        image: toRef(props, 'image'),
+        backgroundImage: toRef(props, 'backgroundImage'),
+        backgroundColor: toRef(props, 'backgroundColor'),
+        appearanceMode: toRef(props, 'appearanceMode'),
+        textTitle: toRef(props, 'textTitle'),
+        textBody: toRef(props, 'textBody')
+    });
+
+    const sizeStyle = computed(() => {
+        return {
+            height: `${props.height}px`,
+            width: `${props.height * (DEVICE_SIZE.IPHONE_X_WIDTH / DEVICE_SIZE.IPHONE_X_HEIGHT)}px`
+        };
+    });
 </script>
